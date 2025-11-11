dab0efde93f53a5cfe5227b0f8e7a8e65861ca5a
leenjae96 <leenjae96@raontec.co.kr>
2025-11-12 00:31:58 +0900

    문제풀이 추가: BFSDFS 탐색 문제

---
+++ src/com/leenjae/programmers/bfsDfs/Network.java
@@ -0,0 +1,56 @@
+package com.leenjae.programmers.bfsDfs;
+
+import java.util.*;
+
+/**
+ * 프로그래머스 - 깊이/너비 우선탐색 - level3
+ * 네트워크
+ * 25.11.12
+ * @author leenjae96
+ */
+public class Network {
+    List<Integer>[] connections;
+    public int solution(int n, int[][] computers) {
+        connections = new List[n];
+        for (int i = 0; i < n; i++) {
+            connections[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (computers[i][j] == 1) {
+                    Integer left = i;
+                    Integer right = j;
+
+                    connections[left].add(right);
+                    connections[right].add(left);
+                }
+            }
+        }
+
+        boolean[] reached = new boolean[n];
+        Arrays.fill(reached, false);
+        Queue<Integer> queue = new ArrayDeque<>();
+        int answer = 0;
+        for(int j = 0 ; j < n ; j ++) {
+            if(reached[j]) continue;
+
+            reached[j] = true;
+            queue.add(j);
+            while(!queue.isEmpty()) {
+                int now = queue.poll();
+
+                for(int i = 0 ; i < connections[now].size() ; i++) {
+                    int next = connections[now].get(i);
+                    if(reached[next]) continue;
+
+                    reached[next] = true;
+                    queue.add(next);
+                }
+            }
+
+            answer++;
+        }
+        return answer;
+    }
+}
