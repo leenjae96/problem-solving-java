68f4d0c7a921c1483bc4d1f4382a036ba89b9f1d
leenjae96 <leenjae96@raontec.co.kr>
2026-03-25 11:02:17 +0900

    문제풀이 추가: 그리디1

---
+++ src/com/leenjae/baekjoon/P1026.java
@@ -0,0 +1,44 @@
+package com.leenjae.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 - 실버4
+ * 보물
+ * 그리디
+ *
+ * @author leenjae96
+ * @date 26.03.19
+ */
+public class P1026 {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    void solution() throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+        for(int i = 0 ; i < N ; i++) {
+            answer+= (A[i] * B[N-i-1]);
+        }
+
+        System.out.println(answer);
+    }
+}
